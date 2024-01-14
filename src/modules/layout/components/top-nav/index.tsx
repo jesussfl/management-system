269c2e01d2414855bfe3b611b1093c408c19f3b9
Jesus Lopez <jesussflr@gmail.com>
2024-01-13 22:57:29 -0400

    fix type error

--- src/modules/layout/components/top-nav/index.tsx
+++ src/modules/layout/components/top-nav/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { cn } from '@/lib/utils'
-import { UserNav } from '@/modules/common/components/user-nav/user-nav'
+import UserNav from '@/modules/common/components/user-nav/user-nav'
 import ToogleTheme from '@/modules/common/components/toogle-theme/toogle-theme'
 
 const TopNav = () => {
