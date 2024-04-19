c23d9a84c38f8cfbcd3a7d523cdd80f483342aab
Jesus Lopez <jesussflr@gmail.com>
2024-04-19 08:07:52 -0400

    fix: remove backups folder

--- src/app/(main)/dashboard/lib/actions/admin/index.ts
+++ src/app/(main)/dashboard/lib/actions/admin/index.ts
@@ -52,12 +52,7 @@ export const backup = async () => {
   try {
     const fileName = 'database-backup-' + new Date().valueOf() + '.tar'
     // const backupFilePath = path.resolve('./public/backups', fileName)
-    const backupFilePath = path.join(
-      process.cwd(),
-      'public',
-      'backups',
-      fileName
-    )
+    const backupFilePath = path.join(process.cwd(), 'public', fileName)
     const execAsync = promisify(exec)
 
     // await execAsync(
