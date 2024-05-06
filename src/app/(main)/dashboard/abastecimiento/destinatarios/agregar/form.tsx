a960d75dcf758fec8492fe0b208882f68ed24140
Jesus Lopez <jesussflr@gmail.com>
2024-05-06 09:30:52 -0400

    fix: fix phone input format for ve country

--- src/app/(main)/dashboard/abastecimiento/destinatarios/agregar/form.tsx
+++ src/app/(main)/dashboard/abastecimiento/destinatarios/agregar/form.tsx
@@ -278,7 +278,7 @@ export default function ReceiversForm({ defaultValues }: Props) {
               rules={{
                 required: 'Este campo es requerido',
               }}
-              render={({ field }) => (
+              render={({ field: { ref, ...field } }) => (
                 <FormItem className="items-center flex flex-1 justify-between gap-2">
                   <FormLabel className="mb-3">{`Numero telefónico:`}</FormLabel>
                   <div className="w-[70%]">
@@ -287,6 +287,20 @@ export default function ReceiversForm({ defaultValues }: Props) {
                         country={'ve'}
                         // placeholder="Ingresa tu numero telefónico"
                         {...field}
+                        masks={{
+                          ve: '....-...-....',
+                        }}
+                        onChange={(value: string, data: any) => {
+                          const phoneNumber = value.split(data.dialCode)[1]
+                          const formattedPhoneNumber = `+${
+                            data.dialCode
+                          }-${phoneNumber.slice(0, 4)}-${phoneNumber.slice(
+                            4,
+                            7
+                          )}-${phoneNumber.slice(7)}`
+                          field.onChange(formattedPhoneNumber)
+                        }}
+                        countryCodeEditable={false}
                       />
                     </FormControl>
                     <FormMessage />
