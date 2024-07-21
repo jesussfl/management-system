40e50f4f920e9c697c7124a56c32894645cb94eb
Jesus Lopez <jesussflr@gmail.com>
2024-07-21 19:52:37 -0400

    style(navbar): Implement username and role instead of avatar

--- src/modules/common/components/user-nav/user-nav.tsx
+++ src/modules/common/components/user-nav/user-nav.tsx
@@ -18,6 +18,7 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { SECTION_NAMES } from '@/utils/constants/sidebar-constants'
 import { validateSections } from '@/lib/data/validate-permissions'
+import { FileLock2, LogOut, LucideFileLock2 } from 'lucide-react'
 export default function UserNav() {
   const { data: session } = useSession()
   const [isAuthorized, setIsAuthorized] = useState(false)
@@ -41,38 +42,27 @@ export default function UserNav() {
           variant="ghost"
           className="flex justify-start gap-2 hover:bg-dark-secondary"
         >
-          <Avatar className="h-8 w-8 ">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col justify-start items-start space-y-1">
-            <p className="text-sm font-medium leading-none text-white">
-              {session?.user?.name}
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium text-white leading-none">
+              {session?.user?.nombre}
+            </p>
+            <p className="text-xs leading-none text-gray-200">
+              {`Rol: ${session?.user?.rol_nombre}`}
             </p>
           </div>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {session?.user?.nombre}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {session?.user?.email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuGroup></DropdownMenuGroup>
-        <DropdownMenuSeparator />
         <Link href={`/dashboard/contrasena-administrador`}>
           {isAuthorized && (
             <DropdownMenuItem>
+              <FileLock2 className="mr-2 h-6 w-6" />
               Cambiar contraseña de Administrador
             </DropdownMenuItem>
           )}
         </Link>
         <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut className="mr-2 h-4 w-4" />
           Cerrar Sesión
         </DropdownMenuItem>
       </DropdownMenuContent>
